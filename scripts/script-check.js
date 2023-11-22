a661404d004c27c810128e1db49f2cdd2b62bb04
soumy-11 <53790183+soumy-11@users.noreply.github.com>
2023-11-22 20:17:22 +0600

    Update script-check.js

--- scripts/script-check.js
+++ scripts/script-check.js
@@ -1,7 +1,7 @@
 
 // document.addEventListener('DOMContentLoaded', updateLogo);
 window.addEventListener('resize', updateLogo);
-setTimeout(updateLogo, 0);
+setTimeout(updateLogo, 1000);
 function updateLogo() 
 {
       if (window.matchMedia("(min-width: 615px)").matches) 
