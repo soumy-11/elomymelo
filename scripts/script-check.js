ec046979b465531f617073ef5e731a3aba85085c
soumy-11 <53790183+soumy-11@users.noreply.github.com>
2024-08-17 12:10:42 +0600

    Update script-check.js

--- scripts/script-check.js
+++ scripts/script-check.js
@@ -335,7 +335,7 @@ let fontload = false;
 // Wait for all fonts to be ready
 document.fonts.ready.then(function() {
     // Check if a specific font is loaded
-    document.fonts.load('Roboto').then(function() {
+    document.fonts.load('1em Roboto').then(function() {
         console.log('cus-roboto font has loaded');
         if (window.matchMedia("(max-width: 615px)").matches) { 
     detectCharacter(); fontload = true; }
