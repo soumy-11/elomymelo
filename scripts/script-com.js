92d618e4a98324bbfdd929d800511ab1a2317033
soumy-11 <53790183+soumy-11@users.noreply.github.com>
2024-08-20 17:45:13 +0600

    Add files via upload

--- scripts/script-com.js
+++ scripts/script-com.js
@@ -403,7 +403,7 @@ function outscale()
         else 
         { 
             document.body.style.overflow = "visible"; 
-            if (!fcone) { setTimeout(doso, 2500); fcone = true; tran5.style.visibility = "visible"; }
+            if (!fcone) { setTimeout(() => { doso(); fcone = true; tran5.style.visibility = "visible"; }, 2500); }
             tran1.style.visibility = "visible"; tran2.style.visibility = "visible"; tran3.style.visibility = "visible"; 
             if (mediain) { tran4.style.display = "none"; } else { tran4.style.display = "block"; } 
 
