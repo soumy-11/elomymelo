69f74fd467a02eb6d04c1ec05658a2e4fac20d09
soumy-11 <53790183+soumy-11@users.noreply.github.com>
2025-03-28 02:24:18 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -19,16 +19,17 @@ self.addEventListener('push', event => {
 
   const options = {
     body: data.body || "Dive into the ultimate showdown between JBL Flip 7 and Flip 6 with 10 essential highlights that reveal their differences and upgrades. The Flip 6 introduced the iconic big, bold JBL logo, setting a cool, stylish tone, while the Flip 7 builds on this with a similar striking design.",
-    image: data.image || "https://elomymelo.com/story-images/jbl-charge-6/charge-6-06.jpg",
+    image: data.image || "https://elomymelo.com/story-images/jbl-flip-7v6/flip-7v6-01.jpg",
     icon: data.icon || "https://elomymelo.com/old-images/circle-trans.png",
-    data: { url: data.url || "https://elomymelo.com/web-stories/flip-7v6-story.html" }
+    data: { url: data.url || "https://elomymelo.com/web-stories/flip-7v6-story.html" },
+    tag: Date.now().toString() // Unique tag for each noti 
   };
 
   event.waitUntil(
     Promise.all([
       self.registration.update(), // Check and install new sw.js in the background
       self.registration.showNotification(
-        data.title || "The New JBL Charge 6, 10 features you must know before buying",
+        data.title || "JBL Flip 7 vs Flip 6 | 10 Essential Highlights",
         options
       )
     ])
