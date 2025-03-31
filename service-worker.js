dda6a3e863d82ca0e205b78011f60cdfb4d0f391
soumy-11 <53790183+soumy-11@users.noreply.github.com>
2025-03-31 10:07:34 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
-
 self.addEventListener('install', event => {
   event.waitUntil(self.skipWaiting()); // Activate immediately after install
 });
@@ -19,20 +18,18 @@ self.addEventListener('push', event => {
 
   const options = {
     body: data.body || "Dive into the ultimate showdown between JBL Flip 7 and Flip 6 with 10 essential highlights that reveal their differences and upgrades. The Flip 6 introduced the iconic big, bold JBL logo, setting a cool, stylish tone, while the Flip 7 builds on this with a similar striking design.",
-    vibrate: [200, 100, 200], requireInteraction: true, tag: Date.now().toString(),
+    requireInteraction: true, 
+    tag: Date.now().toString(),
     image: data.image || "https://elomymelo.com/story-images/jbl-flip-7v6/flip-7v6-01.jpg",
     data: { url: data.url || "https://elomymelo.com/web-stories/charge-6vf7-story.html" },
     icon: data.icon || "https://elomymelo.com/old-images/circle-trans.png"
   };
 
   event.waitUntil(
-    Promise.all([
-      self.registration.update(), // Check and install new sw.js in the background
-      self.registration.showNotification(
-        data.title || "JBL Flip 7 vs Flip 6 | 10 Essential Highlights",
-        options
-      )
-    ])
+    self.registration.showNotification(
+      data.title || "JBL Flip 7 vs Flip 6 | 10 Essential Highlights",
+      options
+    )
   );
 });
 
@@ -40,4 +37,3 @@ self.addEventListener('notificationclick', event => {
   event.notification.close();
   event.waitUntil(clients.openWindow(event.notification.data.url));
 });
-
