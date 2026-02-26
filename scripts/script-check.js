3b4dc41c4a7f8f819c0a20d593395271aa48778c
soumy-11 <53790183+soumy-11@users.noreply.github.com>
2026-02-26 22:02:57 +0600

    Update script-check.js

--- scripts/script-check.js
+++ scripts/script-check.js
@@ -321,7 +321,7 @@ function messEles() {
 }
 
 // Run conditionally
-if (!subMenuSP && window.innerWidth > 615) { messEles(); }
+if (!subMenuSP && window.innerWidth > 615) { messEles(); htmlMess = true; }
 
 // for last-line balance
 function insertAndMeasureSpan(paraTag) 
@@ -382,6 +382,7 @@ function detectCharacter()
 
     setTimeout(heightcheck, 1000); setTimeout(heightcheck, 3000); 
     // document ends here ---------
+
 
 
 
